bd36626dca7f3ebf4bb13391caea78944ce8326c
Mukul <mukulz2020@gmail.com>
2022-03-30 00:25:20 +0530

    minor changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import SocketsProvider from '../context/Socket.context'
 import UserIdProvider from '../context/UserId.context'
@@ -7,11 +8,17 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<UserIdProvider>
-			<SocketsProvider>
-				<Component {...pageProps} />
-			</SocketsProvider>
-		</UserIdProvider>
+		<>
+			<Head>
+				<title>ZAMES</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			</Head>
+			<UserIdProvider>
+				<SocketsProvider>
+					<Component {...pageProps} />
+				</SocketsProvider>
+			</UserIdProvider>
+		</>
 	)
 }
 
